1ce29191b214b54cca90810eb3ee4655df6aafb9
faizisheikh700 <faizisheikh700@gmail.com>
2024-12-08 22:16:02 +0500

    Hackathone2

--- src/components/ShopBlowHero.tsx
+++ src/components/ShopBlowHero.tsx
@@ -4,24 +4,24 @@ export default function BlowHero(){
     return (
         <>
      <div className="showBlowBar w-[100%] h-[4rem] bg-[#F9F1E7]" >
-        <div>
+        <div className="firstRow">
         <Image src={Filter} width={30} alt="Picture of the author" />
             <p>Filter</p>
 
 
-            <p>| Showing 1–16 of 32 results</p>
+            <p>|   Showing 1–16 of 32 results</p>
         </div>
 
 
 
-        <div>
+        <div className="secondRow">
             <p>Show</p>
             {/* <div > 16</div> */}
             
-            </div>
-            <input className="w-[2.5rem] h-[2.5rem] bg-black text-white text-center self-center" type="text" placeholder="16" name="" id="" />
+            <input className="w-[2.5rem] h-[2.5rem] bg-gray-150 text-white text-center self-center" type="text" placeholder="16" name="" id="" />
             <p>Short by</p>
-            <input className="w-[8rem] h-[2.5rem] bg-black text-white text-center self-center" type="text" placeholder="16" name="" id="" />
+            <input className="w-[8rem] h-[2.5rem] bg-gray-150  text-white text-center self-center" type="text" placeholder="16" name="" id="" />
+            </div>
               </div>
         </>
     )
