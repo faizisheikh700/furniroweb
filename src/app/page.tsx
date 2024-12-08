2ebf2c33fb2fd779dfb85df6b385018cd24e5761
faizisheikh700 <faizisheikh700@gmail.com>
2024-12-08 09:34:53 +0500

    hackthone2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Products from "@/components/Products";
+import SubHero from "@/components/Subhero";
 
 export default function Home(){
   return (
     <>
     <Header/>
     <Hero/>
+    <SubHero/>
+    <Products/>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/Products.tsx
@@ -0,0 +1,12 @@
+export default function Products(){
+    return (
+        <>
+        <div>
+            <h2>Our Product</h2>
+            <div>
+                
+            </div>
+        </div>
+        </>
+    )
+}
